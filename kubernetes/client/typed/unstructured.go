0ebe96b9a21b58d9f0645efe41f4d853c3338721
Xinzhao Xu <z2d@jifangcheng.com>
2022-12-08 14:10:01 +0800

    Add Unstructured implementation for typed client

--- kubernetes/client/typed/unstructured.go
+++ kubernetes/client/typed/unstructured.go
@@ -2,35 +2,31 @@ package typed
 
 import (
 	"context"
-	"fmt"
 
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	genericclient "github.com/iawia002/lia/kubernetes/client/generic"
 )
 
-type typedClient struct {
+type unstructuredTypedClient struct {
 	client  client.Reader
 	gvk     schema.GroupVersionKind
 	obj     runtime.Object
 	listObj runtime.Object
 }
 
-// NewTypedClient returns a new Client implementation.
-func NewTypedClient(gvk schema.GroupVersionKind, opts ...func(*options)) (Client, error) {
+// NewUnstructuredTypedClient returns a new Client implementation that returns all objects as Unstructured objects.
+func NewUnstructuredTypedClient(gvk schema.GroupVersionKind, opts ...func(*options)) (Client, error) {
 	o := &options{}
 	for _, f := range opts {
 		f(o)
 	}
 
-	if o.scheme == nil {
-		o.scheme = clientgoscheme.Scheme
-	}
 	if o.cache == nil {
 		if o.config == nil {
 			inClusterConfig, err := rest.InClusterConfig()
@@ -39,30 +35,24 @@ func NewTypedClient(gvk schema.GroupVersionKind, opts ...func(*options)) (Client
 			}
 			o.config = inClusterConfig
 		}
-		cache, err := genericclient.NewClient(o.config, genericclient.WithScheme(o.scheme))
+		cache, err := genericclient.NewClient(o.config)
 		if err != nil {
 			return nil, err
 		}
 		o.cache = cache
 	}
 
-	var (
-		obj     runtime.Object
-		listObj runtime.Object
-	)
-	if o.scheme.Recognizes(gvk) {
-		obj, _ = o.scheme.New(gvk)
-	}
-	listGVK := schema.GroupVersionKind{
+	obj := &unstructured.Unstructured{}
+	obj.SetGroupVersionKind(gvk)
+
+	listObj := &unstructured.UnstructuredList{}
+	listObj.SetGroupVersionKind(schema.GroupVersionKind{
 		Group:   gvk.Group,
 		Version: gvk.Version,
 		Kind:    gvk.Kind + "List",
-	}
-	if o.scheme.Recognizes(listGVK) {
-		listObj, _ = o.scheme.New(listGVK)
-	}
+	})
 
-	return &typedClient{
+	return &unstructuredTypedClient{
 		client:  o.cache,
 		gvk:     gvk,
 		obj:     obj,
@@ -70,14 +60,8 @@ func NewTypedClient(gvk schema.GroupVersionKind, opts ...func(*options)) (Client
 	}, nil
 }
 
-var resourceNotRegisteredError = "kind %s is not registered in scheme"
-
 // Get retrieves an object for the given object key.
-func (t *typedClient) Get(ctx context.Context, key types.NamespacedName, opts ...client.GetOption) (client.Object, error) {
-	if t.obj == nil {
-		return nil, fmt.Errorf(resourceNotRegisteredError, t.gvk.String())
-	}
-
+func (t *unstructuredTypedClient) Get(ctx context.Context, key types.NamespacedName, opts ...client.GetOption) (client.Object, error) {
 	obj := t.obj.(client.Object)
 	if err := t.client.Get(ctx, key, obj, opts...); err != nil {
 		return nil, err
@@ -86,11 +70,7 @@ func (t *typedClient) Get(ctx context.Context, key types.NamespacedName, opts ..
 }
 
 // List retrieves list of objects for a given namespace and list options.
-func (t *typedClient) List(ctx context.Context, namespace string, opts ...client.ListOption) (client.ObjectList, error) {
-	if t.listObj == nil {
-		return nil, fmt.Errorf(resourceNotRegisteredError, t.gvk.String())
-	}
-
+func (t *unstructuredTypedClient) List(ctx context.Context, namespace string, opts ...client.ListOption) (client.ObjectList, error) {
 	listObj := t.listObj.(client.ObjectList)
 	if err := t.client.List(ctx, listObj, append(opts, client.InNamespace(namespace))...); err != nil {
 		return nil, err
