c548123a3a17c47092d9a0831d7a3b818a3183cf
Xinzhao Xu <z2d@jifangcheng.com>
2023-07-05 10:58:40 +0800

    Deprecate multiple functions since Go 1.21

--- maps/maps.go
+++ maps/maps.go
@@ -1,6 +1,7 @@
 package maps
 
 // DeepCopy returns a copy of the original map object.
+// Deprecated: Use the built-in maps.Clone function instead
 func DeepCopy[K comparable, V any](src map[K]V) map[K]V {
 	re := make(map[K]V, len(src))
 	for k, v := range src {
